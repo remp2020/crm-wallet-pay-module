27e3fbe0e676694e2d1cbfc798d4267a55b5f9cc
Miroslav Svítok <mirosvitok@gmail.com>
2022-11-11 17:50:04 +0100

    Fix of laravel-mix incompatibility with Apple Silicon chipset
    - Fixed by temporarily disabling laravel-mix notifications
    - See https://github.com/laravel-mix/laravel-mix/issues/3027

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 let mix = require('laravel-mix');
 
 mix.js('src/assets/src/wallet-pay.js', 'src/assets/dist/js/')
+    // 2022 - temp fix of laravel-mix incompatibility with Apple Silicon
+    // see https://github.com/laravel-mix/laravel-mix/issues/3027
+    .disableNotifications()
     .setPublicPath('src/assets/dist')
     .version();
